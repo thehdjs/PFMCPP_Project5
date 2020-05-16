7d9a751f46d2b0c518d8242290048b1557eb7a73
thehdjs <thehdjs@hotmail.com>
2020-05-16 15:24:17 +0000

    First try

--- main.cpp
+++ main.cpp
@@ -45,28 +45,266 @@ send me a DM to check your pull request
  Wait for my code review.
  */
 
+ #include <iostream>
 /*
  copied UDT 1:
  */
+struct Knife
+{
+    float pointiness, sharpness, thickness, price;
+    unsigned int length;
+    struct Incision
+    {
+        float depth, length;
+        
+        Incision();
+
+        float getHurtLevel();
+    };
+
+    Knife();
+
+    ~Knife();
+    
+    void cut(Incision prettyNastyCut);
+  
+    void stab();
+    
+    void unscrew();
+};
+
+Knife::Incision::Incision()
+{
+    depth = 10;
+    length = 50;
+}
+
+float Knife::Incision::getHurtLevel()
+{
+    return depth + length;
+}
+
+Knife::Knife() :
+pointiness {0.85f},
+sharpness {0.3f},
+thickness {1.5f},
+price {75},
+length {150}
+{}
+
+Knife::~Knife()
+{
+    std::cout << "a Knife is being destroyed\n";
+}
+
+void Knife::cut(Incision prettyNastyCut)
+{
+    prettyNastyCut.getHurtLevel();
+    std::cout << "cut pain level: " << prettyNastyCut.getHurtLevel() << "\n";
+}
+
+void Knife::stab()
+{
+    Incision newIncision;
+    newIncision.getHurtLevel();
+    std::cout << "pointiness and sharpness respectively: " << pointiness << ", " << sharpness << "\n"; 
+}
+
+void Knife::unscrew()
+{
+    std::cout << "pointiness: " << pointiness << "\n";
+}
 
 /*
  copied UDT 2:
  */
+struct Light
+{
+    int temperature = 2500;
+    int luminosity {1000};
+    int electricalConsumption {500};
+    float size, price;
 
+    Light();
+    
+    ~Light();
+    
+    bool turnOn();
+   
+    void setTheMood();
+
+    float addedQuality();
+
+    float fade(float intensity);
+};
+
+Light::Light()
+{
+    size = 25.3f;
+    price = 1000;
+}
+
+Light::~Light()
+{
+    std::cout << "a Light is being destroyed\n";
+}
+bool Light::turnOn()
+{
+    return true;
+}
+
+void Light::setTheMood()
+{
+    luminosity /= 3;
+    temperature = 1000;
+    electricalConsumption /= 2;
+}
+
+float Light::fade(float intensity)
+{
+    while (luminosity < intensity)
+    {
+        luminosity ++;
+        std::cout << "incrementing luminosity\ncurrent level: " << luminosity << " target: " << intensity << "\n";
+        if (luminosity >= intensity) 
+        {
+            std::cout << "fade finished\n";
+            return 100;
+        }   
+    }
+    return luminosity;
+}
 /*
  copied UDT 3:
  */
+struct SwissArmyKnife
+{
+   
+    float weight, size, price;
+    int numberOfCuttingTools, numberOfGrabbingTools;
+
+    SwissArmyKnife();
+
+    ~SwissArmyKnife();
+
+    void cut(Knife::Incision aNewCut);
+
+    void open();
+   
+    void unscrew();
+};
+
+SwissArmyKnife::SwissArmyKnife() : 
+weight (351.3f),
+size (100),
+price (59.99f),
+numberOfCuttingTools (3),
+numberOfGrabbingTools (2)
+{}
+
+SwissArmyKnife::~SwissArmyKnife()
+{
+    std::cout << "a SwissArmyKnife is being destroyed\n";
+}
+void SwissArmyKnife::cut(Knife::Incision aNewCut)
+{
+    aNewCut.getHurtLevel();
+    std::cout << "cut pain level: " << aNewCut.getHurtLevel() << "\n";
+}
+
+void SwissArmyKnife::open()
+{
+    std::cout << "#/cutting tools: " << numberOfCuttingTools << "\n";
+    std::cout << "size: " << size << "\n";
+}
+
+void SwissArmyKnife::unscrew()
+{
+    std::cout << "#/grabbing tools: " << numberOfGrabbingTools << "\n";
+    std::cout << "size: " << size << "\n";
+}
 
 /*
  new UDT 4:
  */
+struct KnifeShop
+{
+    Knife regularKnife;
+    SwissArmyKnife leatherman;
 
+    KnifeShop() {}
+    
+    ~KnifeShop();
+
+    void sell();
+
+    bool isOpen ();
+};
+
+KnifeShop::~KnifeShop()
+{
+    leatherman.open();
+    std::cout << "exiting KnifeShop\n";
+}
+
+void KnifeShop::sell()
+{
+    std::cout << "Knife sold\n";
+}
 /*
  new UDT 5:
  */
 
+struct KillRoom
+{
+    Knife butchersKnife;
+    Light neon;
+
+    KillRoom(){}
+
+    ~KillRoom();
+
+    void getCleaned();
+};
+
+KillRoom::~KillRoom()
+{
+    getCleaned();
+    std::cout << "exiting Killroom\n";
+}
+void KillRoom::getCleaned()
+{
+    std::cout << "KillRoom is getting cleaned\n";
+    neon.fade(1005);
+}
+
 #include <iostream>
 int main()
 {
+    Knife machete;
+    Knife::Incision prettyNastyCut;
+    
+    std::cout << "machete ";
+    
+    machete.cut(prettyNastyCut);
+
+    machete.stab();
+    
+    Light LEDpanel;
+
+    std::cout << "Price of LEDpanel: " << LEDpanel.price << "\n";
+
+    LEDpanel.fade(1003);
+
+    SwissArmyKnife vic;
+    Knife::Incision aNewCut;
+
+    std::cout << "vic ";
+    
+    vic.cut(aNewCut);
+
+    KnifeShop onThirdStreet;
+    KillRoom myBasement;
+    
     std::cout << "good to go!" << std::endl;
 }
